14ece77ecb0f5c9b3e04102b186359aee79a010e
Mindstine <mindstine222@gmail.com>
2025-10-14 05:07:51 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -58,4 +58,9 @@
     host: "::",
     port: 8080,
   },
+    plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   });
