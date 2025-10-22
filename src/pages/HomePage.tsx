09dab15a77872d17eb1fdeb78243475b31a14657
Mindstine <mindstine222@gmail.com>
2025-10-22 20:05:30 +0500

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,23 +1,19 @@
 import { Hero } from '../components/Hero';
 import { Features } from '../components/Features';
-import { Services } from '../components/Services';
 import { Offers } from '../components/Offers';
 import { Testimonials } from '../components/Testimonials';
 import { CTABanner } from '../components/CTABanner';
-import { ScrollToTop } from '../components/ScrollToTop';
-import { WhatsAppButton } from '../components/WhatsAppButton';
+import { FloatingActions } from '../components/FloatingActions';
 
 export function HomePage() {
   return (
     <>
       <Hero />
       <Features />
-      <Services />
       <Offers />
       <Testimonials />
       <CTABanner />
-      <ScrollToTop />
-      <WhatsAppButton />
+      <FloatingActions />
     </>
   );
 }
