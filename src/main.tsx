1afcaac9ccd9d8bc1297fe55a5b7a4a6eb0b579a
Mindstine <mindstine222@gmail.com>
2025-10-24 17:38:50 +0500

    Refactor main.tsx to remove BrowserRouter
    Removed BrowserRouter wrapper from the render method.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // ✅ add this
-import App from "./App.tsx";
-import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+  import { createRoot } from "react-dom/client";
+  import App from "./App.tsx";
+  import "./index.css";
+
+  createRoot(document.getElementById("root")!).render(<App />);
