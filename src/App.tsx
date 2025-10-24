a7c013a26fc4657e6cff6356c8beef96c3ae0fba
Mindstine <mindstine222@gmail.com>
2025-10-24 17:34:24 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,27 @@ import { UmrahPage } from './pages/UmrahPage';
 import { ToursPage } from './pages/ToursPage';
 import { TrackingPage } from './pages/TrackingPage';
 import { SignInPage } from './pages/SignInPage';
+import { OffersPage } from './pages/OffersPage';
+import { CarRentalPage } from './pages/CarRentalPage';
+import { MyAccountPage } from './pages/MyAccountPage';
 
 export default function App() {
   return (
     <Router>
       <div className="min-h-screen bg-white">
         <Header />
-        <main>
+        <main className="relative z-0">
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/visas" element={<VisasPage />} />
             <Route path="/flights" element={<FlightsPage />} />
             <Route path="/umrah" element={<UmrahPage />} />
             <Route path="/tours" element={<ToursPage />} />
+            <Route path="/car-rental" element={<CarRentalPage />} />
+            <Route path="/offers" element={<OffersPage />} />
             <Route path="/tracking" element={<TrackingPage />} />
             <Route path="/signin" element={<SignInPage />} />
+            <Route path="/account" element={<MyAccountPage />} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </main>
