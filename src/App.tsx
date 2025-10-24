8126bd0d6e26789c147d0014ca44471c8debc26e
Mindstine <mindstine222@gmail.com>
2025-10-24 17:42:30 +0500

    Rename route from '/flights' to '/tickets'

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ export default function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/visas" element={<VisasPage />} />
-            <Route path="/flights" element={<Tickets />} />
+            <Route path="/tickets" element={<FlightsPage />} />
             <Route path="/umrah" element={<UmrahPage />} />
             <Route path="/tours" element={<ToursPage />} />
            
